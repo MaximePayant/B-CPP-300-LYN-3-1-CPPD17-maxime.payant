927ddc61b88c85be2891ad3e124f2e38744cbbe9
Maxime Payant <maxime@pop-os.localdomain>
2021-01-22 10:32:48 +0100

    ex00

---
+++ ex00/find.hpp
@@ -0,0 +1,20 @@
+/*
+** EPITECH PROJECT, 2021
+** CPP_SEMINAR
+** File description:
+** find.cpp
+*/
+
+#ifndef FIND_HPP
+#define FIND_HPP
+
+#include <algorithm>
+#include <iterator>
+
+template <class T>
+auto do_find(const T& list, int toFind)
+{
+    return (std::find(list.begin(), list.end(), toFind));
+}
+
+#endif // FIND_HPP
